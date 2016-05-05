46482504fa9e53e372b0c7b06c2ceddf38bf6567
Zoro <zoro@onepiece.com>
2016-05-05 13:07:37 +0800

    Use Router

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,44 +1,17 @@
 from django.conf.urls import url, include
+from snippets import views
 
-from rest_framework.urlpatterns import format_suffix_patterns
-
-from rest_framework import renderers
-from snippets.views import api_root, SnippetViewSet, UserViewSet
-
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight'    
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-
-user_list = UserViewSet.as_view({
-    'get': 'list'    
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'    
-})
+from rest_framework.routers import DefaultRouter
 
 
-urlpatterns = format_suffix_patterns([
-    # /
-    url(r'^$', api_root, name='root'),
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'usrs', views.UserViewSet)
 
-    url(r'^snippets/$', snippet_list, name='snippet-list'),
-    url(r'^snippets/(?P<pk>\d+)/$', snippet_detail, name='snippet-detail'),
-    url(r'^snippets/(?P<pk>\d+)/highlight/$', snippet_highlight, name='snippet-highlight'),
-
-    url(r'^users/$', user_list, name='user-list'),
-    url(r'^users/(?P<pk>\d+)/$', user_detail, name='user-detail'),
-])
-
-urlpatterns += [
+# The API URLs are now determined automatically by the router.
+# Additionally, we include the login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',namespace='rest_framework')),        
 ]
