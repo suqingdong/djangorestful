c0d8287c4d7769e90c1157c260fdb4cf96835bdf
Zoro <zoro@onepiece.com>
2016-05-05 14:55:07 +0800

    Tutorial Fianl

--- snippets/views.py
+++ snippets/views.py
@@ -1,21 +1,15 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer, UserSerializer
-
-from rest_framework import generics
-
 from django.contrib.auth.models import User
 
-from rest_framework import permissions
-from .permissions import IsOwnerOrReadOnly
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import detail_route, api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
-from rest_framework import renderers
+from rest_framework import renderers, generics, viewsets, permissions
 
-from rest_framework import viewsets
-from rest_framework.decorators import detail_route
 
 
 @api_view(['GET'])
